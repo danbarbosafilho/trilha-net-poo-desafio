7aa384bdaa9ed0e4b0ff3e036c0cea6dcfab2fd1
Daniel Barbosa <danielbarbosaflh@gmail.com>
2023-12-08 22:06:20 -0300

    Desafio concluídogit status!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "12345", modelo: "Modelo 1", imei: "111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("App WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "345678", modelo: "Modelo 5i", imei: "222222", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("App WhatsApp");
